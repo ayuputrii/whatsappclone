45f12a76e302fd4286bb70e77d56b6755af89467
ayuputrii <ayuarmadani.pm@gmail.com>
2023-09-03 12:21:31 +0700

    Config import on commons

---
+++ src/components/commons/index.js
@@ -0,0 +1,13 @@
+import CardChat from './cardChat';
+import Button from "./button";
+import Photo from "./photo";
+import Gap from './gap';
+import ImageWithNotFound from './imageWithNotFound';
+
+export {
+  CardChat,
+  Button,
+  Photo,
+  Gap,
+  ImageWithNotFound
+}
