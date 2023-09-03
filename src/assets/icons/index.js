fb65439c650aca5c8955db02c906c36f2343a5aa
ayuputrii <ayuarmadani.pm@gmail.com>
2023-09-03 12:18:48 +0700

    Config import on assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,55 @@
+import IconArrowLeft from './ic_arrow_left_line.svg'
+import IconArrowLeftDown from './ic_arrow_left_down.svg'
+import IconSearch from './ic_search.svg'
+import IconDots from './ic_dots.svg'
+import IconTeam from './ic_team.svg'
+import IconTeamInActive from './ic_team_inactive.svg'
+import IconCamera from './ic_camera.svg'
+import IconCameraWhite from './ic_camera_white.svg'
+import IconArrowRightUpLine from './ic_arrow_right_up_line.svg'
+import IconCall from './ic_call.svg'
+import IconVideoCall from './ic_vc.svg'
+import IconVideoCallGreen from './ic_vc_green.svg'
+import IconHappy from './ic_happy.svg'
+import IconSend from './ic_send.svg'
+import IconMic from './ic_mic.svg'
+import IconRead from './ic_check_read.svg'
+import IconNoRead from './ic_check_no_read.svg'
+import IconDotsGray from './ic_dots_gray.svg'
+import IconArrowLeftGray from './ic_arrow_left_gray.svg'
+import IconCallGreen from './ic_call_green.svg'
+import IconShare from './ic_share.svg'
+import IconInfo from './ic_info.svg'
+import IconArrowRightLine from './ic_arrow_right_s_line.svg'
+import IconMessage from './ic_message.svg'
+import IconBlock from './ic_block.svg'
+import IconDislike from './ic_dislike.svg'
+
+export {
+    IconArrowLeft,
+    IconArrowLeftDown,
+    IconSearch,
+    IconTeam,
+    IconDots,
+    IconTeamInActive,
+    IconCamera,
+    IconCameraWhite,
+    IconArrowRightUpLine,
+    IconCall,
+    IconVideoCall,
+    IconVideoCallGreen,
+    IconHappy,
+    IconSend,
+    IconMic,
+    IconRead,
+    IconNoRead,
+    IconDotsGray,
+    IconArrowLeftGray,
+    IconCallGreen,
+    IconShare,
+    IconInfo,
+    IconArrowRightLine,
+    IconMessage,
+    IconBlock,
+    IconDislike
+}
---
+++ src/assets/img/index.js
@@ -0,0 +1,11 @@
+import BackgroundChat from './bg_chat.png'
+import PhotoProfile from './photo_profile.jpeg'
+import PhotoProfileNotFound from './photo_profile_not_found.png'
+import ImageNotFound from './img_not_found.png'
+
+export {
+  BackgroundChat,
+  PhotoProfile,
+  PhotoProfileNotFound,
+  ImageNotFound
+}
