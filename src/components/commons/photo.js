62460f3f6133c38d38e16203da6ce9b8cc4418a4
ayuputrii <ayuarmadani.pm@gmail.com>
2023-09-03 12:21:44 +0700

    Add photo on commons

---
+++ src/components/commons/photo.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Image, StyleSheet, View } from 'react-native'
+import { PhotoProfileNotFound } from '../../assets'
+
+const Photo = ({uri, imgStyle}) => {
+  return (
+    <React.Fragment>
+      {uri ? (
+        <Image source={{uri: uri}} style={[styles.photoProfile, imgStyle]} />
+      ) : (
+        <Image source={PhotoProfileNotFound} style={styles.photoProfile} />
+      )}
+    </React.Fragment>
+  )
+}
+
+const styles = StyleSheet.create({
+  photoProfile: {
+    width: 50, 
+    height: 50, 
+    borderRadius: 50, 
+    marginRight: 16,
+    resizeMode: "cover"
+  },
+})
+
+export default Photo
