8afc677bcecf83731499a8bdd843f46c22c4ca02
ayuputrii <ayuarmadani.pm@gmail.com>
2023-09-03 12:23:43 +0700

    Add colors on utils

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,14 +1,14 @@
 export const colors = {
     grey: '#231F20',
     gray: '#7e8689',
-    red: '#E82529',
+    red: '#D22F58',
     white: '#FFF',
     black: '#000000',
-    lightGray: '#7e8689',
     inActiveGreen: '#7aa8a2',
     teealGreenDark: '#075e54',
     tealGreen: '#008069',
     lightBlack: '#4C4C4C',
-    lighGreen: '#25D366'
+    lighGreen: '#25D366',
+    msgGreen: '#e7ffdb'
   };
   
