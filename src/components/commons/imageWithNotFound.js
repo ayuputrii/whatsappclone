2071266fa68767871c8a2b85f1861e60a06595ba
ayuputrii <ayuarmadani.pm@gmail.com>
2023-09-03 12:21:14 +0700

    Add imageWithNotFound on commons

---
+++ src/components/commons/imageWithNotFound.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+import { ImageNotFound } from '../../assets'
+
+const ImageWithNotFound = ({photo, imgStyle}) => {
+  return (
+    <React.Fragment>
+      {photo ? (
+        <Image source={photo} style={[styles.img, imgStyle]} />
+      ) : (
+        <Image source={ImageNotFound} style={styles.img} />
+      )}
+    </React.Fragment>
+  )
+}
+
+const styles = StyleSheet.create({
+  img: {
+    width: 70, 
+    height: 70, 
+    marginRight: 2,
+    resizeMode: "cover"
+  },
+})
+
+export default ImageWithNotFound
